010d41b06b58a26d48345958971e4edea179b624
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-02-16 20:26:06 -0800

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programe that prints a line with put function.
+ *
+ * return: must be 0 (end)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
