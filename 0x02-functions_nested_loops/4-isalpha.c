e3ba0f0944e9df103faaf2ac47f4bc8a392a955f
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-02-21 15:12:17 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: Is a character
+ * Return: 1 if is an alphabet and 0 if not
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
