3b48b1f374ab7cd63a90789f24847ceb71bcb5f5
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-02-21 15:33:06 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#nclude "main.h"
+#include "main.h"
 
 /**
  * print_sign - Entry point
@@ -7,12 +7,12 @@
  */
 int print_sign(int n)
 {
-	if (n > 0;)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0;)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
