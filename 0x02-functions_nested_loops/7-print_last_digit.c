de6f9ecf8bf2dc33de63b73037d9af874d9086cf
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-02-21 16:37:46 -0800

    digir

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @n: The number
+ * Return:  the value of last digit
+ */
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ld < 0)
+		ld *= -1;
+
+	_putchar(ld + '0');
+
+	return (0);
+}
