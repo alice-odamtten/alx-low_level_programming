662e0b89f7448c41880a7c8e473fa83cf5a8415d
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-02-23 11:30:29 -0800

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0 i < 10 i++)
+	{
+		for (i = 0 i < 14 i++)
+		{
+			_putchar(i + '0')
+		}
+	}
+	_putchar('\n')
+}
