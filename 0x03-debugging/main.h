a351ffbd1fd65dae97da888ca0e3365b94ac33db
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-02-24 02:35:28 -0800

    main1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdioh>
+#ifndef main_h
+
+#define main_h
+
+#include <stdio.h>
 
 void positive_or_negative(int i);
 void print_remaining_days(int month, int day, int year);
