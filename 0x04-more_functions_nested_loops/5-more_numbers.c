c83fee7c357c4128c2c849ffb784c757d4e82fee
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-02-24 06:01:02 -0800

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,20 @@
  */
 void more_numbers(void)
 {
-	int i;
-	int j;
+int i, j, tens, unit;
 
-	for (i = 0 i < 10 i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0 j <= 14 j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			tens = j / 10;
+			unit = j % 10;
+			if (j >= 10)
+			{
+				_putchar(tens + '0');
+			}
+			_putchar(unit + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
