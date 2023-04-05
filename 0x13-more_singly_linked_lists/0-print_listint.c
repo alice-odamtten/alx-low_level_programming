4c95b8ea4e306356a912aa0658a00b77b0c3d660
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-04-05 16:55:56 -0700

    a function that prints all the elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,14 +14,14 @@
 size_t print_listint(const listint_t *h)
 {
 
-	size_t count = 0;	
+	size_t counte = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		counte++;
 	}
 
-	return (count);
+	return (counte);
 }
