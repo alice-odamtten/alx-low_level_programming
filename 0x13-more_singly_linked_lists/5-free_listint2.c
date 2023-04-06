1ff3ac1d8fbd9c2981f3f34f4f37402bf79fd124
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-04-06 04:50:42 -0700

    frees a node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,33 +9,13 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *last;
+	listint_t last;
 
-	if (*head == NULL)
+	while (head != NULL)
 	{
-		return (NULL);
-	}
-	temp = (listint_t *)malloc(sizeof(listint_t));
-	if (temp == NULL)
-	{
-		return (NULL);
-	}
-	temp = *head;
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	last = (listint_t *)malloc(sizeof(listint_t));
-	if (last == NULL)
-	{
-		return (NULL);
-	}
-	last = head;
-	while (last != NULL)
-	{
-		temp = last->next;
+		last = head;
+		head = head->next;
 		free(last);
-		last = temp;
 	}
-	*head = NULL;
+	head = NULL;
 }
