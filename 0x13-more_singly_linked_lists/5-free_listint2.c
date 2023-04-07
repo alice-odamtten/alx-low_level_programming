c3eb1f17397964e3a7bb5001bf84f0670ce721fb
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-04-07 16:21:08 -0700

    frees a node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t last;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
-		last = head;
-		head = head->next;
+		last = *head;
+		*head = *head->next;
 		free(last);
 	}
 	head = NULL;
