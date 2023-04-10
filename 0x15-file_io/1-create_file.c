993e2ac25fbb6050fa920a0d7e4db72c25987666
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-04-10 15:33:30 -0700

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_TRUSR | S_TWUSR);
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC | 0600);
 	if (fp == -1)
 	{
 		return (-1);
@@ -35,6 +35,11 @@ int create_file(const char *filename, char *text_content)
 			close(fp);
 			return (-1);
 		}
+	}
+	else
+	{
+		close(fp);
+		return (-1);
 	}
 
 	close(fp);
