f197e5e3c1f8558db86de6d65498ff60dd77ed84
alice-odamtten <sakyibeaoamtten@gmail.com>
2023-05-08 20:10:57 -0700

    a function that creates a file.

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 	fp = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	if (fp == -1)
 	{
 		return (-1);
 	}
