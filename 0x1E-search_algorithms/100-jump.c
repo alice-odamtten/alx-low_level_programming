1acfbfa011a8d37710ff062eafc8680873d8362c
alice-odamtten <sakyibeaoamtten@gmail.com>
2023-10-12 09:35:06 -0700

    fixed

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -27,12 +27,12 @@ int jump_search(int *array, size_t size, int value)
 		{
 			break;
 		}
-		printf("Value checked array[%d] = [%d] \n", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 	}
 	printf("Value found between indexes [%d] and [%d]\n", i - jump, i);
 	for (j = low; j < (int)size; j++)
 	{
-		printf("Value checked array[%d] = [%d] \n", j, array[j]);
+		printf("Value checked array[%d] = [%d]\n", j, array[j]);
 		if (array[j] == value)
 		{
 			return (j);
